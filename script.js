f39c57cb65b799820b69b0ba2e1d78d1880c9f5f
DRodrigues17 <adanielluiz71@gmail.com>
2022-02-14 13:13:35 -0300

    adicionando arquivo js

---
+++ script.js
@@ -0,0 +1,20 @@
+function buscarPersonagem() {
+    const idPersonagem = document.getElementById("id_personagem").value
+
+    $.ajax({
+        url: "https://rickandmortyapi.com/api/character/" + idPersonagem,
+        type: "get",
+        success: function (result) {
+            document.getElementById("imagem_personagem").src = result.image
+
+            const textoResultado = `
+            <ul>
+                <li> ${result.name}</li>
+                <li> ${result.status}</li>
+                <li> ${result.species}</li>
+            </ul>
+            `
+            document.getElementById("resultado").innerHTML = textoResultado
+        }
+    });
+}
